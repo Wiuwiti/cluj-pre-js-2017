1ef89485b5294fbf8565798c5587dec3c9fd2eb9
arts0ul@hotmail.com <arts0ul@hotmail.com>
2017-08-26 14:09:02 +0300

    Rename parameters and variables in Evaluation.js

--- src/views/evaluation/Evaluation.js
+++ src/views/evaluation/Evaluation.js
@@ -1,8 +1,8 @@
-const InsertHeader = function(option = {}){
-     return option.map((element) => `<th>${element}</th>`).join('')
+const InsertHeader = function(tableHeaderContent = {}){
+     return tableHeaderContent.map((element) => `<th>${element}</th>`).join('')
 }
 
-const TableHeader = function(option = {}){
+const TableHeader = function(tableHeaderContent = {}){
     return `
     <colgroup>
         <col class="table-button">
@@ -11,73 +11,78 @@ const TableHeader = function(option = {}){
     </colgroup>
     <tr>
         <th></th>
-        ${InsertHeader(option)}
+        ${InsertHeader(tableHeaderContent)}
         <th> </th>
         <th> </th>
     </tr>`
 }
 
-const TableRows = function(option = {}){
+const TableRows = function(tableContent = {}){
     return `
     <tr class="border-bottom">
         <td class= "td-theme"></td>
-        <td class= "td-theme">${option.nume}</td>
-        <td class= "td-theme">${option.technologie}</td>
-        <td class= "td-theme">${option.nivel}</td>
+        <td class= "td-theme">${tableContent.nume}</td>
+        <td class= "td-theme">${tableContent.technologie}</td>
+        <td class= "td-theme">${tableContent.nivel}</td>
         <td class="detail-button td-theme">Detail</td>
         <td class= "td-theme">
-            <img src="${option.buttonIMG}" alt="More Deatails">
+            <img src="${tableContent.buttonIMG}" alt="More Deatails">
         </td>
     </tr>
     `
 }
 
 
-const TableConstructor = function (option ={} , Header ={} ){
+const TableConstructor = function (tableContent ={} , tableHeaderContent ={} ){
     const result = []
-    result.push(TableHeader(Header))
-    result.push(option.map(TableRows).join(''))
+    result.push(TableHeader(tableHeaderContent))
+    result.push(tableContent.map(TableRows).join(''))
     return result.join('')
 }
 
-const TableBody = function(option = {}, tableHeader = {}){
+const TableBody = function(tableContent = {}, tableHeaderContent = {}){
     return `
     <table cellspacing="0" cellpadding= "0" class = "evaluation-table evaluation-table-theme" >
-        ${TableConstructor(option, tableHeader)}
+        ${TableConstructor(tableContent, tableHeaderContent)}
     </table>
     `
 }
 
 
-const EvaluationPage = function(option ={}){
-    return `${NavigationBar(option.headerContent, option.logoContent)}
-    ${TableBody(option.tableContent, option.tableHeader)}
-    ${FooterConstructor(option.footerContent)}`
+const EvaluationPage = function(pageContent ={}){
+    return `${NavigationBar(pageContent.evaluationHeaderContent, pageContent.evaluationLogoContent)}
+    ${TableBody(pageContent.evaluationTableContent, pageContent.evaluationTableHeader)}
+    ${FooterConstructor(pageContent.evaluationFooterContent)}`
 }
 
 window.onload = function(){
-    const result = [];
-    const headerContent = [{
-                            link: "Evaluationv2.html",
-                            text: "Evaluation",
-                            activePage: "navigation-bar-active-page navigation-bar-active-page-theme"
-                        },
-                        {
-                            link: "NewEvaluationv2.html",
-                            text: "New Evaluation"
-                        },
-                        {
-                            link: "Loginv2.html",
-                            text: "Logout",
-                            rightSidedButton: "float-right-element"
-                        }];
+    const evaluationPage = [];
 
-    const logoContent = {link : "assets/logo-v2.jpg",
-                    alt: "Company logo"};
+    const evaluationHeaderContent = [
+        {
+            link: "Evaluationv2.html",
+            text: "Evaluation",
+            activePage: "navigation-bar-active-page navigation-bar-active-page-theme"
+        },
+        {
+            link: "NewEvaluationv2.html",
+            text: "New Evaluation"
+        },
+        {
+            link: "Loginv2.html",
+            text: "Logout",
+            rightSidedButton: "float-right-element"
+        }
+    ];
+
+    const evaluationLogoContent = {
+        link : "assets/logo-v2.jpg",
+        alt: "Company logo"
+    };
 
    
-    const tableHeader = ["Nume", "Technologie", "Nivel"];
-    tableContent = [
+    const evaluationTableHeader = ["Nume", "Technologie", "Nivel"];
+    const evaluationTableContent = [
         {
             nume: "Popescu Adrian",
             technologie: "Javascript",
@@ -103,12 +108,18 @@ window.onload = function(){
             buttonIMG : "/assets/button.jpg"
         }
     ];
-    const footerContent = {footerText : "SoftVision@2017"};
-    const option = {tableContent,
-            tableHeader, 
-            footerContent, 
-            headerContent,
-            logoContent};
-    result.push(EvaluationPage(option))
-    document.querySelector('#app').innerHTML = result;
+
+    const evaluationFooterContent = {
+        footerText : "SoftVision@2017"
+    };
+
+    const options = {
+        evaluationTableContent,
+        evaluationTableHeader, 
+        evaluationFooterContent, 
+        evaluationHeaderContent,
+        evaluationLogoContent
+    };
+    evaluationPage.push(EvaluationPage(options))
+    document.querySelector('#app').innerHTML = evaluationPage;
 }
