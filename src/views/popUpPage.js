28cc6fe88e14244a908b8bfe1f06140998a7221a
Daniel Bogdan <arts0ul@hotmail.com>
2017-09-01 11:26:51 +0300

    output object candidate info in popup page

--- src/views/popUpPage.js
+++ src/views/popUpPage.js
@@ -2,10 +2,20 @@ const showContent = function(object){
     
     const popPage = window.open("", "Evaluation details", "width=1400,height=500")
     clearWindow(popPage)
-    popPage.document.write("<body>"+JSON.stringify(object)+"</body>")
+    const obj = object
+    //console.log(obj.inputCandidate)
+    popPage.document.write(`<body> ${evaluationDetails(obj.inputCandidate)} </body>`)
 }
 
 const clearWindow = function(object){
     object.document.getElementsByTagName('body')[0].innerHTML = ''
-    console.log("was here")
+}
+
+const evaluationDetails = function(array){
+    const candidateContent = array[0]
+    return `
+    <label for="firstname"> Candidate Name: </label><input type="text" name="firstname" value="${candidateContent.candidate}"></input><br>
+    <label for="lastname"> Interviewer name: </label><input type="text" name="lastname" value="${candidateContent.interviewer}"></input><br>
+    <label for="date"> Examination Date: </label><input type="date" name="date" value="${candidateContent.date}"></input>
+    `
 }
