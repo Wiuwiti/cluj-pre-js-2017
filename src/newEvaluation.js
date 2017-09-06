359377536d26ded77f83104806cb2318f784ed25
Daniel Bogdan <arts0ul@hotmail.com>
2017-09-06 14:34:46 +0300

    Create NewEvaluation page using Alex's project structure

--- src/newEvaluation.js
+++ src/newEvaluation.js
@@ -1,8 +1,21 @@
 (function(){
     const render = function(container){
-        container.innerHTML = `Potato`;
+        container.innerHTML = `${NewEvaluationPage(GetNewEvaluationPageData())}`;
     }
 
+    const NewEvaluationPageGrid = function (option = {}, obj = {}){
+        return `	
+        <div class = "mainBlock">
+
+        </div>
+        `
+    }
+
+    const NewEvaluationPage = function(options = {}){
+        return `${NavigationBar(options.newEvaluationHeaderContent, options.newEvaluationLogoContent)}
+        ${NewEvaluationPageGrid(options)}
+        ${FooterConstructor(options.newEvaluationFooterContent)}`
+    }
 
     interviewApp.newEvaluation = {
         init: function(container){
